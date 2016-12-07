8a6bf676aeb92e2b78fd8c3825b004b1d1e098d7
zhaochuang <zhaochuang@example.com>
2016-12-08 00:20:01 +0800

    xiugai

--- Tutorial05/MyNsTest.h
+++ Tutorial05/MyNsTest.h
@@ -19,12 +19,30 @@ class Player : public Noesis::BaseComponent
 {
 public:
 	Player() {}
-	Player(NsString name,NsString imageName) : _name(name), _imageName(imageName){}
+	Player(NsString name) : _name(name), _imageName("shenjian.png"){
+		Noesis::Ptr<Noesis::BitmapImage> image = *new Noesis::BitmapImage("shenjian.png");
+		auto x = image->GetDpiX();
+		auto y = image->GetDpiY();
+		auto width = image->GetWidth();
+		auto height = image->GetHeight();
+		FILE *f;
+		fopen_s(&f, "resource\\shenjian.png", "rb");
+		auto buffer_size = fseek(f, 0, SEEK_END);
+		rewind(f);
+		NsByte * buffer = static_cast<NsByte *>(malloc(sizeof(unsigned char)*buffer_size));
+		fread(buffer, 1, buffer_size, f);
+		Noesis::Ptr<Noesis::BitmapSource> source = Noesis::BitmapSource::Create(
+			width, height, x, y, buffer, buffer_size, width / 8);
+		// @BUG: buffer is not BGRA32 formatted. to be solved
+		_brush = *new Noesis::ImageBrush(source.GetPtr());
+	}
 
 private:
 	NsString _name;
 	NsString _imageName;
+	//Noesis::Ptr<Noesis::BitmapImage> _image;
 	//BitmapImage _image;
+	Noesis::Ptr<Noesis::ImageBrush> _brush;
 
 	NS_IMPLEMENT_INLINE_REFLECTION(Player, BaseComponent)
 	{
@@ -32,7 +50,7 @@ private:
 		NsProp("Name1", &Player::_name);
 		NsProp("Name2", &Player::_name);
 		NsProp("ImageName", &Player::_imageName);
-		//NsProp("Image"), &Player::_image);
+		NsProp("Image", &Player::_brush);
 	}
 
 };
@@ -44,16 +62,16 @@ public:
 	{
 		_players = *new Noesis::ObservableCollection<Player>;
 		std::vector<NsString> nameVector{"Player1","Player2","Player3","Player4","Player5","Player6","Player7","Player8","Player9","Player10","Player11","Player12","Player13","Player14", "Player15"};
-		std::vector<NsString> imageName{"shenjian.png","dianchui.png"};
+		//std::vector<NsString> imageName{"shenjian.png","dianchui.png"};
 		for (NsInt i = 0; i < nameVector.size(); i++) {
-			NsString imgName = "";
-			if (i % 2 == 0) {
+			//NsString imgName = "";
+			/*if (i % 2 == 0) {
 				imgName = imageName[0];
 			} 
 			else {
 				imgName = imageName[1];
-			}
-			Noesis::Ptr<Player> player = *new Player(nameVector[i], imgName);
+			}*/
+			Noesis::Ptr<Player> player = *new Player(nameVector[i]);
 			_players->Add(player.GetPtr());
 		}
 	}
